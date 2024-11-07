4d25547006519a45640508a588db19d5c9aa044f
Cort Cox <cort.cox@students.snow.edu>
2024-11-06 18:25:44 -0700

    Inital commit.

---
+++ src/LogInButton.tsx
@@ -0,0 +1,42 @@
+import { useEffect } from "react";
+import { useAuth } from "react-oidc-context";
+
+function LogInButton() {
+    const auth = useAuth();
+    
+    //set token
+    //set token to date expiration
+    // useEffect{() => {
+    //     console.log("New user.");
+    //     const data
+    //     document.cookie = `auth_token=${}`
+    // }}
+
+    switch (auth.activeNavigator) {
+        case "signinSilent":
+            return <div>Signing you in...</div>;
+        case "signoutRedirect":
+            return <div>Signing you out...</div>;
+    }
+
+    if (auth.isLoading) {
+        return <div>Loading...</div>;
+    }
+
+    if (auth.error) {
+        return <div>Oops... {auth.error.message}</div>;
+    }
+
+    if (auth.isAuthenticated) {
+        return (
+        <div>
+            Hello {auth.user?.profile.sub}{" "}
+            <button onClick={() => void auth.removeUser()}>Log out</button>
+        </div>
+        );
+    }
+
+    return <button onClick={() => void auth.signinRedirect()}>Log in</button>;
+}
+
+export default LogInButton;
---
+++ src/main.tsx
@@ -0,0 +1,25 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.tsx'
+import { AuthProvider, AuthProviderProps } from 'react-oidc-context';
+
+const uri = import.meta.env.VITE_REDIRECT_URI;
+
+if (!uri) console.log("VITE_API_URL is not set");
+else console.log("VITE_API_URL is: " + { uri });
+
+const oidcConfig: AuthProviderProps = {
+  authority: "https://auth.snowse.duckdns.org/realms/advanced-frontend/",
+  client_id: "cort-id",
+  redirect_uri: `${uri}`,
+  // ...
+};
+
+createRoot(document.getElementById('root')!).render(
+  <AuthProvider {...oidcConfig}>
+    <StrictMode>
+      <App />
+    </StrictMode>,
+  </AuthProvider>
+)
